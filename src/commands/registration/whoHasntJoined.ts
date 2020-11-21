dd2407b59a592167665d602883e68e29bcc40164
jason-s-yu <jyu031103@gmail.com>
2020-11-20 23:05:09 -0800

    who hasn't joined command

---
+++ src/commands/registration/whoHasntJoined.ts
@@ -0,0 +1,57 @@
+import { Command, CommandoMessage } from 'discord.js-commando';
+import { prisma } from '../../bot';
+
+export default class WhoHasntJoinedCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'whohasntjoined',
+      group: 'registration',
+      memberName: 'whohasntjoined',
+      description: 'Retrieve who has not joined',
+      args: [
+        {
+          key: 'school',
+          prompt: 'What school?',
+          type: 'string'
+        }
+      ],
+      userPermissions: ['ADMINISTRATOR']
+    });
+  }
+
+  run = async (message: CommandoMessage, { school }) => {
+    interface SchoolOptions {
+      [key: string]: any
+    }
+    let schoolOptions: SchoolOptions = {
+      school
+    };
+    if (school === 'all') {
+      delete schoolOptions.school;
+    }
+
+    const user = await prisma.user.findMany({
+      where: {
+        ...schoolOptions,
+        OR: [
+          {
+            relation: 'Student'
+          },
+          {
+            relation: 'Volunteer'
+          }
+        ],
+        joined: false
+      }
+    });
+    if (!user || user.length < 1) return message.say(`Group \`${school}\` not found.`);
+
+    let build = '';
+
+    user.map(person => {
+      build += `${person.firstName} ${person.lastName} | ${person.email}\n`;
+    });
+
+    return message.say(`__**Users not joined**__\n\n${build}`);
+  }
+}
